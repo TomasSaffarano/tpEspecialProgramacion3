a3e17c6441bbce481fa65863c67424826726f549
ampirosell <129613071+ampirosell@users.noreply.github.com>
2026-05-26 17:10:58 -0300

    Create Servicios.java

---
+++ tpEspecialProg3/src/servicios/Servicios.java
@@ -0,0 +1,35 @@
+package servicios;
+
+public class Servicios {
+
+    /*Completar con las estructuras y métodos privados que se
+   * requieran
+    *
+     * Expresar la complejidad temporal del constructor.
+     */
+    public Servicios(String pathCamiones, String pathPaquetes)
+    {
+    }
+    /*
+     * Expresar la complejidad temporal del servicio 1.
+     */
+    public Paquete servicio1(String codigoPaquete) {
+        //TO DO
+        return null;
+    }
+    /*
+     * Expresar la complejidad temporal del servicio 2.
+     */
+    public List<Paquete> servicio2(boolean contieneAlimentos) {
+        //TO DO
+        return null;
+    }
+    /*
+     * Expresar la complejidad temporal del servicio 3.
+     */
+    public List<Paquete> servicio3(int urgenciaMinima, int
+            urgenciaMaxima) {
+        //TO DO
+        return null;
+    }
+}
